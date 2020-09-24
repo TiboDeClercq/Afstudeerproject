4410ff3c7ccf8fd587ad72884fdb73772a05de20
TiboDeClercq <tibo.declercq@student.ucll.be>
2020-09-24 12:17:36 +0200

    Error and succes poppup

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import socket
 from tkinter import *
+from tkinter import messagebox
+
 from scan import scan
 
 ipList = []
 def addIP():
     entered_text=textentry.get()
+    if not entered_text:
+        warningPopUp("You haven't entered an IP addres")
     return valid_ip(entered_text)
 
 #function to check if entered IP address is valid
@@ -14,56 +18,77 @@ def valid_ip(address):
 
         #if the entered IP address is not in the list -> add to list
         if address in ipList:
-            print('Ip already in list')
+            warningPopUp("This IP address is already in the list.")
         else:
             ipList.append(address)
             output.insert(END, address)
             return True
     except:
+        warningPopUp("This IP address is not valid.")
         return False
 
 #function to start the scan
 def sendscan():
-    scan(deviceEntry.get(), ipList)
+    if not deviceEntry.get():
+        warningPopUp("You haven't entered a device name.")
+    else:
+
+        scan(deviceEntry.get(), ipList)
+
+#function to show warning message
+def warningPopUp(msg):
+    warningPopUp = Tk()
+    warningPopUp.wm_title("Warning !")
+    warningPopUp.geometry("320x150")
+    label = Label(warningPopUp, text="Warning: " + msg)
+    label.pack(side="top", fill="x", pady=30)
+    B1 = Button(warningPopUp, text="Close", command = warningPopUp.destroy )
+    B1.pack()
+    warningPopUp.mainloop()
+
+#function to show waiting message
+def succesPopUp(msg):
+    succesPopUp = Tk()
+    succesPopUp.wm_title("Succes message")
+    succesPopUp.geometry("320x150")
+    label = Label(succesPopUp, text="The scan has succesfully started.")
+    label.pack(side="top", fill="x", pady=30)
+    B1 = Button(succesPopUp, text="Okay", command = succesPopUp.destroy )
+    B1.pack()
+    succesPopUp.mainloop()
 
 #layout
-window = Tk()
-window.title('OpenVAS automated scanner')
+root = Tk()
+root.title('OpenVAS automated scanner')
 
 #label and input for IP address
-Label (window, text="Ip address", bg="black", fg="white", font="none 12 bold").grid(row=0,column=0, sticky=W)
-textentry = Entry(window, width= 20, bg="white")
+Label (root, text="Ip address", fg="black", font="12").grid(row=0,column=0, sticky=W)
+textentry = Entry(root, width= 20, bg="white")
 textentry.grid(row=1,column=0,sticky=W)
 
 #list of IP addresses
-output = Listbox(window, width=20, height=6,  background="white")
-output.grid(row=0, column=4, sticky=W, rowspan=3)
+output = Listbox(root, width=20, height=6,  background="white")
+output.grid(row=0, column=4, sticky=W, rowspan=6)
 
 #button to add IP address to list
-Button(window, text="Add address", width=7, command=addIP).grid(row=2, column=0, sticky=W)
+Button(root, text="Add address", width=7, command=addIP).grid(row=2, column=0, sticky=W)
 
 #label and input for target name
-Label (window, text="Device name", bg="black", fg="white", font="none 12 bold").grid(row=3,column=0, sticky=W)
-deviceEntry = Entry(window, width= 20, bg="white")
+Label (root, text="Device name", fg="black", font="12").grid(row=3,column=0, sticky=W)
+deviceEntry = Entry(root, width= 20, bg="white")
 deviceEntry.grid(row=4,column=0,sticky=W)
 
-<<<<<<< HEAD
-Button(window, text="Scan", width=6, command=scan).grid(row=8, column=0, sticky=W)
-window.mainloop()
-=======
-
-
 #Radiobuttons to choose deep scan or normal scan
-Label (window, text="Type of scan",  fg="black", font="none 12").grid(row=5,column=0, sticky=W)
+Label (root, text="Type of scan",  fg="black", font="none 12").grid(row=5,column=0, sticky=W)
 selected = IntVar()
 
-rad1 = Radiobutton(window,text='Normal scan', value=1, variable=selected, width=20)
-rad2 = Radiobutton(window,text='Hard scan', value=2, variable=selected, width=20)
+rad1 = Radiobutton(root,text='Normal scan', value=1, variable=selected, width=20)
+rad2 = Radiobutton(root,text='Hard scan', value=2, variable=selected, width=20)
 
 rad1.grid(column=0, row=6)
 rad2.grid(column=0, row=7)
-
+    
 #button to start the scan
-Button(window, text="Scan", width=6, command=sendscan).grid(row=8, column=0, sticky=W)
-window.mainloop()
->>>>>>> 2f1b0c331422c3c828b8906e08be3b8f27e71e58
+Button(root, text="Scan", width=6, command=sendscan).grid(row=8, column=0, sticky=W)
+Button(root, text="Pop", width=6, command=succesPopUp).grid(row=9, column=0, sticky=W)
+root.mainloop()
