b7e85abd26ea6960e241f72b4e61d6365f3a9873
TiboDeClercq <tibo.declercq@student.ucll.be>
2020-09-25 13:30:57 +0200

    extracten gelukt

--- nmap_test/nmap_script.py
+++ nmap_test/nmap_script.py
@@ -1,16 +1,32 @@
-import os
 import sys
 import re
-
+from tempfile import mkstemp
+from shutil import move, copymode
+from os import fdopen, remove
+import os
 # print("Scanning subnet")
-# os.system('nmap -sn -oA nmap_test/nmap-subnet 127.0.0.0/24 | grep Up nmap_test/nmap-subnet.gnmap | cut -d " " -f 2 > nmap_test/live-hosts.txt')
+#os.system('nmap -sn -oA 127.0.0.0/24 | grep Up  | cut -d " " -f 2 > nmap_test/live-hosts.txt')
 
-# print("Scanning open ports")
-# os.system("nmap -p- -oA nmap_test/nmap-ports 127.0.0.1 | grep open | cut -d " " -f1 > nmap_test/ports.txt")
+#Scanning all ports
+print("Scanning open ports...")
+os.system('nmap -p-  127.0.0.1 | grep open | cut -d" " -f1 > nmap_test/ports.txt')
 
 with open("nmap_test/ports.txt", "r") as f:
     inhoud = f.read()
-    # m = re.match('[0-9]+(/tcp)', inhoud)
-    # print(m.group(0))
-    # for match in re.findall(r'.*(\/tcp)', inhoud):
-    print(re.findall(r'.*(\/tcp)', inhoud))
+    #Create temp file
+    fh, abs_path = mkstemp()
+    with fdopen(fh,'w') as new_file:
+        new_file.write("T:")
+        for match in re.findall(r'.*\/tcp', inhoud):
+            new_file.write(match[:4]+ ", ")
+        new_file.write("\nU:")
+        for match in re.findall(r'.*\/udp', inhoud):
+            new_file.write(match[:4]+ ", ")
+        
+
+   #Copy the file permissions from the old file to the new file
+    copymode("nmap_test/ports.txt", abs_path)
+    #Remove original file
+    remove("nmap_test/ports.txt")
+    #Move new file
+    move(abs_path, "nmap_test/ports.txt")
