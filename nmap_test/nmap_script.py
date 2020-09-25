4531d1e1811b0df1e308b823b5ba62b532874597
TiboDeClercq <tibo.declercq@student.ucll.be>
2020-09-25 14:01:22 +0200

    custom portlist done

--- nmap_test/nmap_script.py
+++ nmap_test/nmap_script.py
@@ -4,8 +4,20 @@ from tempfile import mkstemp
 from shutil import move, copymode
 from os import fdopen, remove
 import os
+#GVM importsfrom gvm.connections import UnixSocketConnection
+from gvm.connections import UnixSocketConnection
+from gvm.protocols.gmp import Gmp
+from gvm.transforms import EtreeTransform
+from gvm.xml import pretty_print
+from gvm.protocols.gmpv9 import CredentialType
+from gvm.protocols.gmpv9 import ScannerType
+from gvm.protocols.gmpv9 import FilterType
+from xml.etree import ElementTree
 # print("Scanning subnet")
 #os.system('nmap -sn -oA 127.0.0.0/24 | grep Up  | cut -d " " -f 2 > nmap_test/live-hosts.txt')
+
+connection = UnixSocketConnection()
+transform = EtreeTransform()
 
 #Scanning all ports
 print("Scanning open ports...")
@@ -19,14 +31,24 @@ with open("nmap_test/ports.txt", "r") as f:
         new_file.write("T:")
         for match in re.findall(r'.*\/tcp', inhoud):
             new_file.write(match[:4]+ ", ")
-        new_file.write("\nU:")
+        new_file.write(" U:")
         for match in re.findall(r'.*\/udp', inhoud):
             new_file.write(match[:4]+ ", ")
-        
-
    #Copy the file permissions from the old file to the new file
     copymode("nmap_test/ports.txt", abs_path)
     #Remove original file
     remove("nmap_test/ports.txt")
     #Move new file
     move(abs_path, "nmap_test/ports.txt")
+
+
+with Gmp(connection, transform=transform) as gmp:
+    # Login -> change to default admin password
+    gmp.authenticate('scanner', 'scanner')
+    
+    
+    with open("nmap_test/ports.txt", "r") as f:
+        inhoud2 = f.read()
+    
+    gmp.create_port_list('Custome ports123', 'T:5432, 9392,  U:')
+    
