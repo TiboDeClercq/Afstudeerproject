c70c1dd05ab9822407eb6e04fce0bb2d2a13ca7d
SamArnalsteen <sam.arnalsteen@gmail.com>
2020-09-25 14:48:15 +0100

    merge

--- nmap_test/nmap_script.py
+++ nmap_test/nmap_script.py
@@ -8,9 +8,9 @@ import re
 # print("Scanning open ports")
 # os.system("nmap -p- -oA nmap_test/nmap-ports 127.0.0.1 | grep open | cut -d " " -f1 > nmap_test/ports.txt")
 
-with open("nmap_test/ports.txt", "r") as f:
+with open("ports.txt", "r") as f:
     inhoud = f.read()
     # m = re.match('[0-9]+(/tcp)', inhoud)
     # print(m.group(0))
     # for match in re.findall(r'.*(\/tcp)', inhoud):
-    print(re.findall(r'.*(\/tcp)', inhoud))
+    print(re.findall(r'.*\/tcp', inhoud))
