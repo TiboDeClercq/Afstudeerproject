42ee89be308b75e208e5d44c3a0bde7bc1670ef4
RubennVdk <vandekerckhove.ruben@hotmail.com>
2020-10-05 22:05:41 +0200

    js still doesnt read response

--- flask/static/css_image/progress.js
+++ flask/static/css_image/progress.js
@@ -5,17 +5,20 @@ setInterval(getprogress, 2000);
 
 function getprogress() {
     getprogressRqst.open("GET", "/prgrss", true);
-    getprogressRqst.onreadystatechange = getProgressData();
+    var serverResponse = getprogressRqst.responseText;
+    console.log("---------------------------- serverresponse:-" + serverResponse + "_");
+    document.getElementById("progrss").innerHTML = serverResponse;
+    //getprogressRqst.onreadystatechange = getProgressData();
     //getProgressData();
     getprogressRqst.send();
 }
 
-function getProgressData() {
-    if (getprogressRqst.readyState === 4) {
-        if (getprogressRqst.status === 200) {
-            var serverResponse = getprogressRqst.responseText;
-            console.log("---------------------------- serverresponse:-" + serverResponse + "_");
-            document.getElementById("progrss").innerHTML = serverResponse;
-        }
-    }
-}
+// function getProgressData() {
+//     if (getprogressRqst.readyState === 4) {
+//         if (getprogressRqst.status === 200) {
+//             var serverResponse = getprogressRqst.responseText;
+//             console.log("---------------------------- serverresponse:-" + serverResponse + "_");
+//             document.getElementById("progrss").innerHTML = serverResponse;
+//         }
+//     }
+// }
