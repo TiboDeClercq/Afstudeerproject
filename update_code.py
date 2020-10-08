afac80a7b1e0bd1df17a9f5443909dc610c14174
RubennVdk <vandekerckhove.ruben@hotmail.com>
2020-10-08 12:23:21 +0200

    for pull

--- update_code.py
+++ update_code.py
@@ -1,7 +1,10 @@
 import os
+import git
 
 def update_code():
     os.system('cd')
     #path is relative, depends on vm path (so might need change on your pc)
-    os.system('cd /home/kali/git_script/Afstudeerproject')
-    os.system('git pull --no-edit https://github.com/TiboDeClercq/Afstudeerproject.git')
+    g = git.cmd.Git('/home/kali/git_script/Afstudeerproject')
+    g.pull()
+    # os.system('cd /home/kali/git_script/Afstudeerproject')
+    # os.system('git pull --no-edit https://github.com/TiboDeClercq/Afstudeerproject.git')
