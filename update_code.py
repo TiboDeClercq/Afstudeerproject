32d876085ea333ca80e49a160eab63dbc1953170
RubennVdk <vandekerckhove.ruben@hotmail.com>
2020-10-08 11:28:21 +0200

    attempt for automatic pull

--- update_code.py
+++ update_code.py
@@ -4,4 +4,4 @@ def update_code():
     os.system('cd')
     #path is relative, depends on vm path (so might need change on your pc)
     os.system('cd ~/git_script/Afstudeerproject')
-    os.system('git pull')
+    os.system('git pull https://github.com/TiboDeClercq/Afstudeerproject.git')
