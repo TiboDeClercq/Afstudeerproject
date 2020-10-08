16f038540b08ebaac83a0fd5339a08250b461cd9
RubennVdk <vandekerckhove.ruben@hotmail.com>
2020-10-08 12:12:23 +0200

    attempt for automatic pull

--- update_code.py
+++ update_code.py
@@ -4,4 +4,5 @@ def update_code():
     os.system('cd')
     #path is relative, depends on vm path (so might need change on your pc)
     os.system('cd /home/kali/git_script/Afstudeerproject')
-    os.system('git pull https://github.com/TiboDeClercq/Afstudeerproject.git')
+    os.system('git fetch https://github.com/TiboDeClercq/Afstudeerproject.git && git merge --no-edit master')
+    #os.system('git merge -m "merge message" --no-edit https://github.com/TiboDeClercq/Afstudeerproject.git')
