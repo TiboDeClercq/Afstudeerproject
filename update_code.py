26c48274d29099b8b01e031a4aa8d3a75858bfc7
RubennVdk <vandekerckhove.ruben@hotmail.com>
2020-10-08 11:38:08 +0200

    attempt for automatic pull

--- update_code.py
+++ update_code.py
@@ -4,4 +4,4 @@ def update_code():
     os.system('cd')
     #path is relative, depends on vm path (so might need change on your pc)
     os.system('cd /home/kali/git_script/Afstudeerproject')
-    os.system('git pull --ff-only https://github.com/TiboDeClercq/Afstudeerproject.git')
+    os.system('git pull https://github.com/TiboDeClercq/Afstudeerproject.git')
